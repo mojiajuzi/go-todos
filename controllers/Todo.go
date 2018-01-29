c857a2f7dc4f6e0cc103bf9c13fdb1004ea94b87
gru <1065407867@qq.com>
2018-01-29 17:22:29 +0800

    finish crud

--- controllers/Todo.go
+++ controllers/Todo.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"errors"
+	"fmt"
 	"hello/models"
 	"net/http"
 	"strconv"
@@ -35,17 +36,44 @@ func Store(c *gin.Context) {
 
 //Show  获取详情
 func Show(c *gin.Context) {
-
+	todoID, _ := strconv.Atoi(c.Param("id"))
+	fmt.Println(todoID)
+	db, err := getDB(c)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": err})
+	}
+	var todo models.TodoModel
+	db.First(&todo, todoID)
+	c.JSON(http.StatusOK, todo)
 }
 
 // Update 更新
 func Update(c *gin.Context) {
+	todoID, _ := strconv.Atoi(c.Param("id"))
 
+	db, err := getDB(c)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": err})
+	}
+	var todo models.TodoModel
+	db.First(&todo, todoID)
+	todo.Title = c.PostForm("title")
+	completed, _ := strconv.Atoi(c.PostForm("completed"))
+	todo.Completed = completed
+	db.Save(&todo)
+	c.JSON(http.StatusOK, todo)
 }
 
 // Destory 删除
 func Destory(c *gin.Context) {
-
+	todoID, _ := strconv.Atoi(c.Param("id"))
+	db, err := getDB(c)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": err})
+	}
+	var todo models.TodoModel
+	db.Delete(&todo, todoID)
+	c.JSON(http.StatusOK, todo)
 }
 
 func getDB(c *gin.Context) (*gorm.DB, error) {
